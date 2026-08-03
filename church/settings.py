7d104ef274086013d3997435db6709a01fe18754
chrismushaz <mushabati6jr@gmail.com>
2026-08-03 23:23:12 +0200

    Fix CSRF settings for Render

--- church/settings.py
+++ church/settings.py
@@ -22,6 +22,10 @@ ALLOWED_HOSTS = os.environ.get(
     "life-celebration-arena.onrender.com,localhost,127.0.0.1"
 ).split(",")
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://life-celebration-arena.onrender.com",
+]
+
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
